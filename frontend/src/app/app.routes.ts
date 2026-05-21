75f6a8bf975231f4aebf215351b1020de7b1468d
Sondos Nazzal <sondos.nazzal@outlook.com>
2026-05-21 22:24:43 +0300

    manually merged the venture-shop-ui with the activity

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,21 +10,23 @@ import { TakeActivityComponent } from './component/activity/take-activity/take-a
 import { ActivityListComponent } from './component/activity/activity-list/activity-list'
 import { SubmissionsComponent } from './component/activity/submissions/submissions';
 import { StudentActivitiesComponent } from './component/student-activities/student-activities';
+import { VentureShop } from "./student/venture-shop/venture-shop";
+import { AdminVentureShop } from "./admin/admin-venture-shop/admin-venture-shop";
 
 export const routes: Routes = [
   { path: 'teacherdachboard', component: TeacherDachboard },
   { path: 'teacherclasses', component: Teacherclasses },
   { path: 'teacherstudent', component: Teacherstudent },
   { path: 'teacherattendance', component: Teacherattendance },
- // { path: 'teacheractivities', component: TeacherActivity },
-//{ path: 'studentactivities', component: StudentActivities },
+  // { path: 'teacheractivities', component: TeacherActivity },
+  //{ path: 'studentactivities', component: StudentActivities },
   { path: 'activities', component: ActivityListComponent },
   { path: 'activities/create', component: CreateActivityComponent },
   { path: 'activities/:id/take', component: TakeActivityComponent },
   { path: '', redirectTo: 'activities', pathMatch: 'full' },
   { path: 'activities/:id/submissions', component: SubmissionsComponent },
-{ path: 'studentactivities',         component: StudentActivitiesComponent },
-{ path: 'activities/:id/edit',   component: CreateActivityComponent },
-
-
+  { path: 'studentactivities', component: StudentActivitiesComponent },
+  { path: 'activities/:id/edit', component: CreateActivityComponent },
+  { path: 'venture-shop', component: VentureShop },
+  { path: 'admin-venture-shop', component: AdminVentureShop },
 ];
---
+++ frontend/src/app/models/shop-item.model.ts
@@ -0,0 +1,8 @@
+export interface ShopItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  emoji: string;
+}
